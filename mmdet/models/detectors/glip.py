4d001f29cefc95fa1c3d658449a79523d4b4ce99
Pikus16 <bpikus@gmail.com>
2024-05-24 17:03:27 +0000

    fix glip

--- mmdet/models/detectors/glip.py
+++ mmdet/models/detectors/glip.py
@@ -28,8 +28,8 @@ def find_noun_phrases(caption: str, verbose: bool = True) -> list:
     """
     try:
         import nltk
-        nltk.download('punkt', download_dir=expanduser('~/nltk_data'), quiet=verbose)
-        nltk.download('averaged_perceptron_tagger', download_dir=expanduser('~/nltk_data'), quiet=verbose)
+        nltk.download('punkt', download_dir=expanduser('~/nltk_data'), quiet=not verbose)
+        nltk.download('averaged_perceptron_tagger', download_dir=expanduser('~/nltk_data'), quiet=not verbose)
     except ImportError:
         raise RuntimeError('nltk is not installed, please install it by: '
                            'pip install nltk.')
