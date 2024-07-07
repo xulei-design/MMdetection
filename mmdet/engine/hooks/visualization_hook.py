633dffe23015aa6264a204425458450886c1345c
冷漠 <lengmo1996@gmail.com>
2024-07-07 16:28:29 +0800

    Update visualization_hook.py

--- mmdet/engine/hooks/visualization_hook.py
+++ mmdet/engine/hooks/visualization_hook.py
@@ -99,8 +99,8 @@ class DetVisualizationHook(Hook):
             img_path = outputs[0].img_path
             img_bytes = get(img_path, backend_args=self.backend_args)
             img = mmcv.imfrombytes(img_bytes, channel_order='rgb')
-            img = mmcv.Resize(outputs[0].scale_factor)
-            
+            img = mmcv.imrescale(img,outputs[0].img_shape)
+
             self._visualizer.add_datasample(
                 osp.basename(img_path) if self.show else 'val_img',
                 img,
